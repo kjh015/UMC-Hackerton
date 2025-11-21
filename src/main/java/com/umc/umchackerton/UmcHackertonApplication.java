26fd3452f70e5962871f0d5faa9b0c10728eddd4
kjh015 <khj843@naver.com>
2025-11-21 21:47:03 +0900

    init

---
+++ src/main/java/com/umc/umchackerton/UmcHackertonApplication.java
@@ -0,0 +1,13 @@
+package com.umc.umchackerton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcHackertonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcHackertonApplication.class, args);
+    }
+
+}
