97624647a1af58bfdabfc58e220576363951a5ad
kjh015 <khj843@naver.com>
2025-11-21 23:53:15 +0900

    cicd Test health 추가

--- src/main/java/com/umc/umchackerton/test/test.java
+++ src/main/java/com/umc/umchackerton/test/test.java
@@ -1,4 +1,13 @@
 package com.umc.umchackerton.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class test {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok, It's work!";
+    }
 }
